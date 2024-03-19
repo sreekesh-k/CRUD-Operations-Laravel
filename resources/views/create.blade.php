4913380fd025ded8e8cabbbae6b71695731d3bd1
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-03-19 21:40:21 +0530

    Bootsrap

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,34 +5,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CREATE</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
-    <center>
-        <h1>CREATE YOUR ITEM</h1>
-        <hr>
-        <div>
-            @if ($errors->any())
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            @endif
+
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">Create Items</span>
         </div>
-        <form action="{{ route('create.confirm') }}" method="POST">
-            @csrf
-            <div>
-                <input type="text" name="name" placeholder="ItemName">
-            </div>
-            <div>
-                <input type="text" name="description" placeholder="Description">
-            </div>
-            <div>
-                <input type="submit" name="submit" value="Confirm">
-            </div>
-        </form>
-    </center>
+    </nav>
+    <div>
+        @if ($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
+    </div>
+    <form action="{{ route('create.confirm') }}" method="POST">
+        @csrf
+        <div>
+            <input type="text" name="name" placeholder="ItemName">
+        </div>
+        <div>
+            <input type="text" name="description" placeholder="Description">
+        </div>
+        <div>
+            <input type="submit" name="submit" value="Confirm">
+        </div>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,35 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+$count = 0;
+?>
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Index</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
-    <center>
-        <h1>Read Items:</h1>
-        <hr>
-        <div>
-            @if (session()->has('success'))
-                <div>
-                    {{ session('success') }}
-                </div>
-            @endif
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">Read Items</span>
         </div>
-        @foreach ($items as $item)
+    </nav>
+    <div>
+        @if (session()->has('success'))
             <div>
-                {{ $item->id }}. {{ $item->name }}
-                <a href="{{ route('updating', ['item' => $item]) }}">edit</a>
-                <a href="{{ route('deleting', ['item' => $item]) }}">Delete</a>
+                {{ session('success') }}
             </div>
-        @endforeach
-        <a href="{{ route('creating') }}">Create items?</a>
-    </center>
+        @endif
+    </div>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">SI</th>
+                <th scope="col">ID</th>
+                <th scope="col">Item</th>
+                <th scope="col">Description</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($items as $item)
+                <tr>
+                    <th scope="row">{{ ++$count }}</th>
+                    <td> {{ $item->id }}</td>
+                    <td> {{ $item->name }}</td>
+                    <td>{{ $item->description }}</td>
+                    <td> <a href="{{ route('updating', ['item' => $item]) }}">Edit</a></td>
+                    <td><a href="{{ route('deleting', ['item' => $item]) }}">Delete</a></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+    <a href="{{ route('creating') }}">Create items?</a>
 
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
